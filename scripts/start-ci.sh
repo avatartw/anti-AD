d218fc153e2dfba9c125305c38321db3def2fea4
avatartw <69660730+avatartw@users.noreply.github.com>
2021-03-10 21:01:10 +0800

    Update start-ci.sh

--- scripts/start-ci.sh
+++ scripts/start-ci.sh
@@ -6,16 +6,44 @@ cd $(cd "$(dirname "$0")";pwd)
 
 easylist=(
   "https://easylist-downloads.adblockplus.org/easylistchina+easylist.txt"
-  "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt"
-  "https://easylist.to/easylist/fanboy-annoyance.txt"
-  "https://easylist.to/easylist/easyprivacy.txt"
+  "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt"#!/bin/bash
+
+source /etc/profile
+
+cd $(cd "$(dirname "$0")";pwd)
+
+easylist=(
+  "https://raw.githubusercontent.com/avatartw/avatartw/main/my-anti-ad.txt"
+  "https://filters.adtidy.org/extension/chromium/filters/101_optimized.txt"
+  "https://filters.adtidy.org/extension/chromium/filters/118_optimized.txt"
+  "https://raw.githubusercontent.com/avatartw/avatartw/main/219_optimized.txt"
+#  "https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt"
+  "https://filters.adtidy.org/extension/chromium/filters/220_optimized.txt"
+#  "https://easylist-downloads.adblockplus.org/fanboy-annoyance.txt"
+  "https://filters.adtidy.org/extension/chromium/filters/122_optimized.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/fanboy-addon/fanboy_annoyance_thirdparty.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/fanboy-addon/fanboy_notifications_thirdparty.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/fanboy-addon/fanboy_social_thirdparty.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/easylist_cookie/easylist_cookie_thirdparty.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/fanboy-addon/fanboy_annoyance_specific_block.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/fanboy-addon/fanboy_notifications_specific_block.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/fanboy-addon/fanboy_social_specific_block.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/easylist_cookie/easylist_cookie_specific_block.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/fanboy-addon/fanboy_annoyance_international.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/fanboy-addon/fanboy_social_international.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/easylist_cookie/easylist_cookie_international_specific_block.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/fanboy-addon/fanboy_annoyance_allowlist.txt"
+#  "https://raw.githubusercontent.com/easylist/easylist/master/easylist_cookie/easylist_cookie_allowlist.txt"
   "https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt"
 )
 
 hosts=(
-  "https://raw.githubusercontent.com/neoFelhz/neohosts/gh-pages/full/hosts.txt"
-  "https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts"
-  "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
+#  "https://raw.githubusercontent.com/avatartw/avatartw/main/hosts-anti-ad.txt"
+#  "https://raw.githubusercontent.com/neoFelhz/neohosts/gh-pages/full/hosts.txt"
+#  "https://raw.githubusercontent.com/jdlingyu/ad-wars/master/hosts"
+#  "https://raw.githubusercontent.com/crazy-max/WindowsSpyBlocker/master/data/hosts/spy.txt"
+#  "https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts"
+#  "https://raw.githubusercontent.com/PolishFiltersTeam/KADhosts/master/KADhosts.txt"
 )
 
 strict_hosts=(
@@ -33,55 +61,55 @@ rm -f ./origin-files/hosts*
 rm -f ./origin-files/strict-hosts*
 rm -f ./origin-files/dead-hosts*
 
-cp ./origin-files/yhosts-latest.txt ./origin-files/hosts1000.txt
+#cp ./origin-files/yhosts-latest.txt ./origin-files/hosts1000.txt
 cp ./origin-files/some-else.txt ./origin-files/dead-hosts444.txt
 
-curl --connect-timeout 60 -s -o - https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanProgramAD.list \
- | grep -F 'DOMAIN-SUFFIX,' | sed 's/DOMAIN-SUFFIX,/127.0.0.1 /g' >./origin-files/hosts999.txt
-curl --connect-timeout 60 -s -o - https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanAD.list \
- | grep -F 'DOMAIN-SUFFIX,' | sed 's/DOMAIN-SUFFIX,/127.0.0.1 /g' >./origin-files/hosts998.txt
+#curl --connect-timeout 60 -s -o - https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanProgramAD.list \
+# | grep -F 'DOMAIN-SUFFIX,' | sed 's/DOMAIN-SUFFIX,/127.0.0.1 /g' >./origin-files/hosts999.txt
+#curl --connect-timeout 60 -s -o - https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/BanAD.list \
+# | grep -F 'DOMAIN-SUFFIX,' | sed 's/DOMAIN-SUFFIX,/127.0.0.1 /g' >./origin-files/hosts998.txt
 
 
 for i in "${!easylist[@]}"
 do
-  echo "开始下载 easylist${i}..."
+  echo "開始下載 easylist${i}..."
   curl -o "./origin-files/easylist${i}.txt" --connect-timeout 60 -s "${easylist[$i]}"
   # shellcheck disable=SC2181
   if [ $? -ne 0 ];then
-    echo '下载失败，请重试'
+    echo '下載失敗，請重試'
     exit 1
   fi
 done
 
 for i in "${!hosts[@]}"
 do
-  echo "开始下载 hosts${i}..."
+  echo "開始下載 hosts${i}..."
   curl -o "./origin-files/hosts${i}.txt" --connect-timeout 60 -s "${hosts[$i]}"
   # shellcheck disable=SC2181
   if [ $? -ne 0 ];then
-    echo '下载失败，请重试'
+    echo '下載失敗，請重試'
     exit 1
   fi
 done
 
 for i in "${!strict_hosts[@]}"
 do
-  echo "开始下载 strict-hosts${i}..."
+  echo "開始下載 strict-hosts${i}..."
   curl -o "./origin-files/strict-hosts${i}.txt" --connect-timeout 60 -s "${strict_hosts[$i]}"
   # shellcheck disable=SC2181
   if [ $? -ne 0 ];then
-    echo '下载失败，请重试'
+    echo '下載失敗，請重試'
     exit 1
   fi
 done
 
 for i in "${!dead_hosts[@]}"
 do
-  echo "开始下载 dead-hosts${i}..."
+  echo "開始下載 dead-hosts${i}..."
   curl -o "./origin-files/dead-hosts${i}.txt" --connect-timeout 60 -s "${dead_hosts[$i]}"
   # shellcheck disable=SC2181
   if [ $? -ne 0 ];then
-    echo '下载失败，请重试'
+    echo '下載失敗，請重試'
     exit 1
   fi
 done
@@ -89,10 +117,10 @@ done
 
 cd origin-files
 
-cat hosts*.txt | grep -v -E "^((#.*)|(\s*))$" \
- | grep -v -E "^[0-9\.:]+\s+(ip6\-)?(localhost|loopback)$" \
- | sed s/0.0.0.0/127.0.0.1/g | sed s/::/127.0.0.1/g | sort \
- | uniq >base-src-hosts.txt
+#cat hosts*.txt | grep -v -E "^((#.*)|(\s*))$" \
+# | grep -v -E "^[0-9\.:]+\s+(ip6\-)?(localhost|loopback)$" \
+# | sed s/0.0.0.0/127.0.0.1/g | sed s/::/127.0.0.1/g | sort \
+# | uniq >base-src-hosts.txt
 
 cat strict-hosts*.txt | grep -v -E "^((#.*)|(\s*))$" \
  | grep -v -E "^[0-9\.:]+\s+(ip6\-)?(localhost|loopback)$" \
@@ -104,12 +132,25 @@ cat dead-hosts*.txt | grep -v -E "^(#|\!)" \
  | uniq >base-dead-hosts.txt
 
 
-cat easylist*.txt | grep -E "^\|\|[^\*\^]+?\^" | sort | uniq >base-src-easylist.txt
+cat easylist*.txt | grep -E "^\|\|[^\*\^\/]+?\^" | grep -v -E "\^\*|\/" | sort | uniq >base-src-easylist.txt
 cat easylist*.txt | grep -E "^\|\|?([^\^=\/:]+)?\*([^\^=\/:]+)?\^" | sort | uniq >wildcard-src-easylist.txt
-cat easylist*.txt | grep -E "^@@\|\|?[^\^=\/:]+?\^([^\/=\*]+)?$" | sort | uniq >whiterule-src-easylist.txt
+cat easylist*.txt | grep -E "^@@?[^\^=\/:]+?\^([^\/=\*]+)?$" | sort | uniq >whiterule-src-easylist.txt
+cat easylist0.txt | grep -E "^[^\|!]|\*|(^[^!]\S*[^\^]$)" | sort | uniq >e-easylist.txt
+cat easylist0.txt | grep -E "^[^@!]\S*[^\^]$" | sort | uniq >>base-src-easylist.txt
+#cat easylist0.txt | grep -E "\$(\S+,)*(client|dnstype|dnsrewrite|important|badfilter|ctag)" | sort | uniq >rule-modifiers.txt
+sort base-src-easylist.txt | uniq >a.txt
+mv -f a.txt base-src-easylist.txt
 
 cd ../
 
 php make-addr.php
+#sleep 2m
+cat ../anti-ad-easylist.txt ./origin-files/e-easylist.txt > ../a.txt
+(head -n 4 ../a.txt && tail -n +5 ../a.txt | sort) | uniq > ../anti-ad-easylist.txt
+rm -f ../a.txt
+
 echo
 php ./tools/easylist-extend.php ../anti-ad-easylist.txt
+#cat ./origin-files/rule-modifiers.txt>> ../anti-ad-easylist.txt
+#(head -n 4 ../anti-ad-easylist.txt && tail -n +5 ../anti-ad-easylist.txt | sort) | uniq > ../a.txt
+#mv -f ../a.txt  ../anti-ad-easylist.txt
