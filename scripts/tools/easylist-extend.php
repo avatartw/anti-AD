f4d573db16f9020a6f0e3cc7e5af193a9f62e044
avatartw <69660730+avatartw@users.noreply.github.com>
2021-03-10 21:02:45 +0800

    Update easylist-extend.php

--- scripts/tools/easylist-extend.php
+++ scripts/tools/easylist-extend.php
@@ -126,64 +126,99 @@ $ARR_MERGED_WILD_LIST = array(
 
 $ARR_REGEX_LIST = array(
     '/9377[a-z]{2}\.com$/' => null,
-    '/^(\S+\.)?ad([\d]|m|s)?\./' => null,
-    '/^(\S+\.)?affiliat(es|ion|e)\./' => null,
+    '/^([^\s\/]+\.)?ad([\d]|m|s)?\./' => null,
+    '/^([^\s\/]+\.)?affiliat(es|ion|e)\./' => null,
     '/afgr[\d]{1,2}\.com$/' => null,
-    '/^(\S+\.)?analytics(\-|\.)/' => null,
-    '/^(\S+\.)?counter(\-|\.)/' => null,
-    '/^(\S+\.)?pixels?\./' => null,
+    '/^([^\s\/]+\.)?analytics(\-|\.)/' => null,
+    '/^([^\s\/]+\.)?counter(\-|\.)/' => null,
+    '/^([^\s\/]+\.)?pixels?\./' => null,
     '/syma[a-z]\.cn$/' => null,
-    '/^(\S+\.)?widgets?\./' => null,
-    '/^(\S+\.)?(web)?stats?\./' => null,
-    '/^(\S+\.)?track(ing)?\./' => null,
-    '/^(\S+\.)?tongji\./' => null,
-    '/^(\S+\.)?toolbar\./' => null,
-    '/^(\S+\.)?adservice\.google\./' => null,
-    '/^(\S+\.)?d[\d]+\.sina(img)?(\.com)?\.cn/' => null,
-    '/^(\S+\.)?sax[\dns]?\.sina\.com\.cn/' => null,
+    '/^([^\s\/]+\.)?widgets?\./' => null,
+    '/^([^\s\/]+\.)?(web)?stats?\./' => null,
+    '/^([^\s\/]+\.)?track(ing)?\./' => null,
+    '/^([^\s\/]+\.)?tongji\./' => null,
+    '/^([^\s\/]+\.)?toolbar\./' => null,
+    '/^([^\s\/]+\.)?adservice\.google\./' => null,
+    '/^([^\s\/]+\.)?d[\d]+\.sina(img)?(\.com)?\.cn/' => null,
+    '/^([^\s\/]+\.)?sax[\dns]?\.sina\.com\.cn/' => null,
     '/delivery([\d]{2}|dom|modo).com$/' => null,
-    '/^(\S+\.)?[c-s]ads(abs|abz|ans|anz|ats|atz|del|ecs|ecz|ims|imz|ips|ipz|kis|kiz|oks|okz|one|pms|pmz)\.com/' => null,
-    '/^(\S+\.)?([a-z\d\-]+\.)?(?!xn--)[^\.\/]{26,}\.(com|net|cn)(\.cn)?$/' => null, //超长域名
-    '/^(\S+\.)?11599[\da-z]{2,20}\.com$/' => null, //"澳门新葡京"系列
-    '/^(\S+\.)?61677[\da-z]{0,20}\.com$/' => null, //"澳门新葡京"系列
-    '/^(\S+\.)?[0-9a-f]{15,}\.com$/' => null, //15个字符以上的16进制域名
-    '/^(\S+\.)?[0-9a-z]{16,}\.xyz$/' => null, //16个字符以上的.xyz域名
-    '/^(\S+\.)?6699[0-9]\.top$/' => null, //连号
-    '/^(\S+\.)?abie[0-9]+\.top$/' => null, //连号
-    '/^(\S+\.)?ad[0-9]{3,}m.com$/' => null, //连号
-    '/^(\S+\.)?aj[0-9]{4,}.online$/' => null, //连号
-    '/^(\S+\.)?xpj[0-9]\.net$/' => null, //连号
-    '/^(\S+\.)?ylx-[0-9].com$/' => null, //连号
-    '/^(\S+\.)?ali2[a-z]\.xyz$/' => null, //连号
-    '/^(\S+\.)?777\-?partners?\.(net|com)$/' => null, //组合
-    '/^(\S+\.)?voyage-prive\.[a-z]+(\.uk)?$/' => null, //组合
-    '/^(\S+\.)?e7[0-9]{2,4}\.(net|com)?$/' => null, //组合
-    '/^(\S+\.)?g[1-4][0-9]{8,9}\.com?$/' => null, //批量组合
-    '/^(\S+\.)?hg[0-9]{4,5}\.com?$/' => null, //批量组合
+    '/^([^\s\/]+\.)?[c-s]ads(abs|abz|ans|anz|ats|atz|del|ecs|ecz|ims|imz|ips|ipz|kis|kiz|oks|okz|one|pms|pmz)\.com/' => null,
+    '/^([^\s\/]+\.)?([a-z\d\-]+\.)?(?!xn--)[^\.\/]{26,}\.(com|net|cn)(\.cn)?$/' => null, //超長域名
+    '/^([^\s\/]+\.)?11599[\da-z]{2,20}\.com$/' => null, //"澳門新葡京"系列
+    '/^([^\s\/]+\.)?61677[\da-z]{0,20}\.com$/' => null, //"澳門新葡京"系列
+    '/^([^\s\/]+\.)?[0-9a-f]{15,}\.com$/' => null, //15個字符以上的16進制域名
+    '/^([^\s\/]+\.)?[0-9a-z]{16,}\.xyz$/' => null, //16個字符以上的.xyz域名
+    '/^([^\s\/]+\.)?6699[0-9]\.top$/' => null, //連號
+    '/^([^\s\/]+\.)?abie[0-9]+\.top$/' => null, //連號
+    '/^([^\s\/]+\.)?ad[0-9]{3,}m.com$/' => null, //連號
+    '/^([^\s\/]+\.)?aj[0-9]{4,}.online$/' => null, //連號
+    '/^([^\s\/]+\.)?xpj[0-9]\.net$/' => null, //連號
+    '/^([^\s\/]+\.)?ylx-[0-9].com$/' => null, //連號
+    '/^([^\s\/]+\.)?ali2[a-z]\.xyz$/' => null, //連號
+    '/^([^\s\/]+\.)?777\-?partners?\.(net|com)$/' => null, //組合
+    '/^([^\s\/]+\.)?voyage-prive\.[a-z]+(\.uk)?$/' => null, //組合
+    '/^([^\s\/]+\.)?e7[0-9]{2,4}\.(net|com)?$/' => null, //組合
+    '/^([^\s\/]+\.)?g[1-4][0-9]{8,9}\.com?$/' => null, //批量組合
+    '/^([^\s\/]+\.)?hg[0-9]{4,5}\.com?$/' => null, //批量組合
+    '/^([\w]{1,4}\.)(\w+)?-?pocztex.*\d+-?(polska)?\.[\w]{2,14}/' => null,
+    '/^([\w]{1,4}\.)?(\w+)?.?\w+-?poczta(\d+)?-?polska(\d+)?\.[\w]{2,14}/' => null,
+    '/^([\w]{1,4}\.)?(\w+)?.?\w+-?polska(\d+)?-?poczta\.[\w]{2,14}/' => null,
+    '/^([\w]{1,4}\.)?(\w+)?-?(\d+)?gwalt(fakty?|news|wiadomosci?|polska|monitoring)\.[\w]{2,14}/' => null,
+    '/^([\w]{1,4}\.)?(\w+)?\.?poczta-?polska\d+\.[\w]{2,14}/' => null,
+    '/^([\w]{1,4}\.)?(\w+)?\.\w+-?pocztex(\d+)?\.[\w]{2,14}/' => null,
+    '/^([\w]{1,4}\.)?(apps?|best|competition|game|mobile|play|prize|reward|sweeps)[\d]{2,8}\.[a-z-]{4,22}[\d]{1,8}\.(agency|icu|life|live|loan)/' => null,
+    '/^([\w]{1,4}\.)?(apps?|best|competition|game|mobile|play|prize|reward|sweeps)\.[a-z-]{4,22}[0-9]{1,8}\.info/' => null,
+    '/^([\w]{1,4}\.)?(copysubstancepattern|decimalprovehour|discusssheetenemy|educationbedring|effectstorestream|fingermilkorgan|flyingintheclouds|goedekans|historyalwayschildren|ideaanstudy|insectrunfollow|jsdevelopmentq|mindspellnothing|noseangerspend|occurthousandlast|payageround-|randalieren|roundsolutionbrought|sciencewhategg|shoutmostface|thinkwhilemelody|thoughcopyforest|togethercleareducation|waterdiepattern|whateyeweight|wildgrelns)[\d]{1,2}\.live/' => null,
+    '/^([^\s\/]+\.)?anzugreifen([1-9]|1[0-5])\.live/' => null,
+    '/^([^\s\/]+\.)?consquarters([1-9]|1[0-5])\.live/' => null,
+    '/^([^\s\/]+\.)?curtainhardsh([1-9]|1[0-9]|2[0-5])\.live/' => null,
+    '/^([^\s\/]+\.)?esoterisch([1-9]|1[0-5])\.live/' => null,
+    '/^([^\s\/]+\.)?garternal([1-9]|1[0-9]|20)\.live/' => null,
+    '/^([^\s\/]+\.)?greenelephants([1-9]|1[0-5])\.live/' => null,
+    '/^([^\s\/]+\.)?headregret([1-9]|1[0-9]|20)\.live/' => null,
+    '/^([^\s\/]+\.)?lifefordatings([1-9]|1[0-5])\.live/' => null,
+    '/^([^\s\/]+\.)?listentome([1-9]|1[0-5])\.live/' => null,
+    '/^([^\s\/]+\.)?matchwhytotal([1-9]|1[0-5])\.live/' => null,
+    '/^([^\s\/]+\.)?rainxpose([1-9]|1[0-9]|20)\.live/' => null,
+    '/^([^\s\/]+\.)?teckinharmux([1-9]|1[0-9]|20)\.live/' => null,
+    '/^([^\s\/]+\.)?tekerstreet([6-9]|1[0-9]|20)\.live/' => null,
+    '/^([^\s\/]+\.)?testing3srv([1-9]|10)\.live/' => null,
+    '/^([^\s\/]+\.)?thvedroisil([1-9]|1[0-5])\.live/' => null,
+    '/^([^\s\/]+\.)?waarschijnlijk([1-9]|1[0-5])\.live/' => null,
+    '/^([\w]{1,4}\.)?.*-?(fakty?|news|wiadomosci?)gwalt(\d+)?\.[\w]{2,14}/' => null,
+    '/^([\w]{1,4}\.)?.*-?emonitoring-?e?poczta[\w]{1,14}/' => null,
+    '/^([\w]{1,4}\.)?\d+platnosci?\.online/' => null,
+    '/^([\w]{1,4}\.)?allegro(finanse)?\d+\.pl/' => null,
+    '/^([\w]{1,4}\.)?faktury\d+\.org/' => null,
+    '/^([\w]{1,4}\.)?(probablerootport)-?[\d]{1,2}\.live/'
 
-    // '/^(\S+\.)?(?=.*[a-f].*\.com$)(?=.*\d.*\.com$)[a-f0-9]{15,}\.com$/' => null,
+    // '/^([^\s\/]+\.)?(?=.*[a-f].*\.com$)(?=.*\d.*\.com$)[a-f0-9]{15,}\.com$/' => null,
 );
 
-//对通配符匹配或正则匹配增加的额外赦免规则
+//對通配符匹配或正則匹配增加的額外赦免規則
 $ARR_WHITE_RULE_LIST = array(
+    '@@||fonts.gstatic.com^' => 1,
+    '@@||edge.activity.windows.com^' => 1,
+    '@@||edge-enterprise.activity.windows.com^' => 1,
     '@@||tongji.*kuwo.cn^' => 0,
     '@@||tracking.epicgames.com^' => 0,
-    '@@||tracker.eu.org^' => 1, //强制加白，BT tracker，有形如2.tracker.eu.org的域
-    '@@||stats.uptimerobot.com^' => 0, //uptimerobot监测相关
-    '@@||track.sendcloud.org^' => 0, //邮件退订域名
-    '@@||log.mmstat.com^' => 0, //修复优酷视频显示禁用了cookie
-    '@@||adm.10jqka.com.cn^' => 0, //同花顺
-    '@@||center-h5api.m.taobao.com^' => 1, //h5页面
+    '@@||tracking.magnetmail.net^' => 0,
+    '@@||tracker.eu.org^' => 1, //強制加白，BT tracker，有形如2.tracker.eu.org的域
+    '@@||stats.uptimerobot.com^' => 0, //uptimerobot監測相關
+    '@@||track.sendcloud.org^' => 0, //郵件退訂域名
+    '@@||log.mmstat.com^' => 0, //修復優酷視頻顯示禁用了cookie
+    '@@||adm.10jqka.com.cn^' => 0, //同花順
+    '@@||center-h5api.m.taobao.com^' => 1, //h5頁面
     '@@||app.adjust.com^' => 1, //https://github.com/AdguardTeam/AdGuardSDNSFilter/pull/186
-    '@@||widget.weibo.com^' => 0, //微博外链
-    '@@||uland.taobao.com^' => 1, //淘宝coupon #83
-    '@@||advertisement.taobao.com^' => 1, //CNAME 被杀，导致s.click.taobao.com等服务异常
+    '@@||widget.weibo.com^' => 0, //微博外鏈
+    '@@||uland.taobao.com^' => 1, //淘寶coupon #83
+    '@@||advertisement.taobao.com^' => 1, //CNAME 被殺，導致s.click.taobao.com等服務異常
     '@@||baozhang.baidu.com^' => 1, //CNAME e.shifen.com 
-    '@@||tongji.edu.cn^' => 1, // 同济大学
-    '@@||tongji.cn^' => 1, // 同济大学 #281
-    '@@||ad.siemens.com.cn^' => 1, // 西门子下载中心
-    '@@||sdkapi.sms.mob.com^' => 1, // 短信验证码 #127
-    '@@||stats.gov.cn^' => 1, // 国家统计局 #144
+    '@@||tongji.edu.cn^' => 1, // 同濟大學
+    '@@||tongji.cn^' => 1, // 同濟大學 #281
+    '@@||ad.siemens.com.cn^' => 1, // 西門子下載中心
+    '@@||sdkapi.sms.mob.com^' => 1, // 短信驗證碼 #127
+    '@@||stats.gov.cn^' => 1, // 國家統計局 #144
     '@@||tj.gov.cn^' => 1,
     '@@||sax.sina.com.cn^' => 1, // #155
     '@@||api.ad-gone.com^' => 1, // #207
@@ -198,15 +233,15 @@ $ARR_WHITE_RULE_LIST = array(
     '@@||storage.live.com^' => 1, // #333
     '@@||skyapi.onedrive.live.com^' => 1, // #333
     '@@||counter-strike.net^' => 1, // #332
-    '@@||ftp.bmp.ovh^' => 1, // #353
+    '@@|ftp.bmp.ovh^|' => 1, // #353
 );
 
-//针对上游赦免规则anti-AD不予赦免的规则，即赦免名单的黑名单
+//針對上游赦免規則anti-AD不予赦免的規則，即赦免名單的黑名單
 $ARR_WHITE_RULE_BLK_LIST = array(
     '@@||ads.nipr.ac.jp^' => null,
 );
 
-//针对上游通配符规则中anti-AD不予采信的规则，即通配符黑名单
+//針對上游通配符規則中anti-AD不予採信的規則，即通配符黑名單
 $ARR_WILD_BLK_LIST = array(
     'cnt*rambler.ru' => null,
     'um*.com' => null,
@@ -321,7 +356,7 @@ while(!feof($src_fp)){
     $line_count++;
 }
 
-//按需写入白名单规则
+//按需寫入白名單規則
 $wrote_whitelist = array();
 $whiterule = file(WHITERULE_SRC, FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
 $whiterule=array_fill_keys($whiterule, 0);
