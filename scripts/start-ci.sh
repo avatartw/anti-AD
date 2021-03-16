3baff01154817d7d7c4a8c7088d78c28f603484a
avatartw <69660730+avatartw@users.noreply.github.com>
2021-03-16 15:22:30 +0800

    Update start-ci.sh

--- scripts/start-ci.sh
+++ scripts/start-ci.sh
@@ -131,8 +131,8 @@ cat easylist*.txt | grep -E "^\|\|[^\*\^\/]+?\^" | grep -v -E "\^\*|\/" | sort |
 cat easylist*.txt | grep -E "^\|\|?([^\^=\/:]+)?\*([^\^=\/:]+)?\^" | sort | uniq >wildcard-src-easylist.txt
 cat easylist*.txt | grep -E "^@@?[^\^=\/:]+?\^([^\/=\*]+)?$" | sort | uniq >whiterule-src-easylist.txt
 cat easylist0.txt | grep -E "^\|\|?([^\^=\/:]+)?\*([^\^=\/:]+)?\^" | sort | uniq >e0-wildcard-whiterule.txt
-cat easylist0.txt | grep -E "^@@?[^\^=\/:]+?\^([^\/=\*]+)?$" | sort | uniq >>e0-wildcard-whiterule.txt
-cat easylist0.txt | grep -E "^[^\|!]|(^[^!]\S*[^\^]$)" | sort | uniq >e-easylist.txt
+cat easylist0.txt | grep -E "^@@?" | sort | uniq >>e0-wildcard-whiterule.txt
+cat easylist0.txt | grep -E "^[^\|!]|(^[^!]\S*[^\^]$)" >e-easylist.txt
 #cat easylist0.txt | grep -E "\$(\S+,)*(client|dnstype|dnsrewrite|important|badfilter|ctag)" | sort -d | uniq >rule-modifiers.txt
 #cat easylist0.txt | grep -E "^[^@!]\S*[^\^]$" | sort | uniq >>base-src-easylist.txt
 #sort base-src-easylist.txt | uniq >a.txt
