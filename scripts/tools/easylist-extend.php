e8efefbf4d6266e0a1242472717094186926937c
avatartw <69660730+avatartw@users.noreply.github.com>
2021-06-19 23:43:49 +0800

    Update easylist-extend.php

--- scripts/tools/easylist-extend.php
+++ scripts/tools/easylist-extend.php
@@ -261,6 +261,8 @@ $ARR_WHITE_RULE_LIST = array(
     '@@||settings-win.data.microsoft.com^' => 1, // #426
     '@@||insideruser.microsoft.com^' => 1, // #426
     '@@||metrics.vrch.at^' => 1, // #440
+    '@@||trackings.post.japanpost.jp^' => 1, // #441
+    '@@||track.aliexpress.com^' => 1, // #446
 );
 
 //針對上游赦免規則anti-AD不予赦免的規則，即赦免名單的黑名單
