e8487845348c168e1bce66c1b032fc7790a962de
avatartw <69660730+avatartw@users.noreply.github.com>
2021-07-25 03:11:45 +0800

    Update easylist-extend.php

--- scripts/tools/easylist-extend.php
+++ scripts/tools/easylist-extend.php
@@ -264,6 +264,7 @@ $ARR_WHITE_RULE_LIST = array(
     '@@||trackings.post.japanpost.jp^' => 1, // #441
     '@@||track.aliexpress.com^' => 1, // #446
     '@@||s.mvconf.f.360.cn^' => 1, // #462
+    '@@||widget.1688.com^' => 1, // #469
 );
 
 //針對上游赦免規則anti-AD不予赦免的規則，即赦免名單的黑名單
