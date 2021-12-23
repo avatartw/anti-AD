668e4eea317db32d1a99cc11809d3013393bacc0
avatartw <69660730+avatartw@users.noreply.github.com>
2021-12-23 12:48:49 +0800

    Update easylist-extend.php

--- scripts/tools/easylist-extend.php
+++ scripts/tools/easylist-extend.php
@@ -22,6 +22,7 @@ define('WHITERULE_SRC', ROOT_DIR . 'origin-files/whiterule-src-easylist.txt');
 
 $ARR_MERGED_WILD_LIST = array(
     'ad*.udn.com' => null,
+    'p*-ad-sign.byteimg.com' => null, // #529
     '*.mgr.consensu.org' => null,
     'vs*.gzcu.u3.ucweb.com' => null,
     'ad*.goforandroid.com' => null,
