07eaebee73d35f5ec6a7bba49cae75ff996a0a8d
GitHub Action <action@github.com>
2024-03-05 21:58:29 +0800

    fixed 893

--- scripts/lib/black_domain_list.php
+++ scripts/lib/black_domain_list.php
@@ -1483,7 +1483,6 @@ return [
     "googletagmanager-cn.com" => ["googletagmanager-cn.com"],
     "googletagservices-cn.com" => ["googletagservices-cn.com"],
     "googlevads-cn.com" => ["googlevads-cn.com"],
-    "gvt1-cn.com" => ["gvt1-cn.com"],
 
     // 批量添加域名
     "0202.com.tw" => ["0202.com.tw", "www.0202.com.tw"],
