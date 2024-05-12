1fa723b8e55bfeb2af7277255e5d1638a208f3e7
GitHub Action <action@github.com>
2024-05-12 20:26:17 +0800

    fixed 918

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -439,6 +439,7 @@ return [
     "ads.cdn.tvb.com" => 0, // #911
     "ads.console.aliyun.com" => 0, // #912
     "deliver.kuwo.cn" => 0, // #917
+    "mcdn.bilivideo.cn" => 0, // #918
 
     "iflow.uczzd.cn" => 0, // uc搞笑段子
     "m.uczzd.cn" => 0, // uc搞笑段子
