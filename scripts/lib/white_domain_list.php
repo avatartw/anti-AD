fe87e88f5ded8126241a9ed8c42e87c1df9cb0a7
GitHub Action <action@github.com>
2024-12-12 18:09:47 +0800

    fixed 998

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -453,6 +453,7 @@ return [
     "awstrack.me" => 0, // #467 #964
     "c.paypal.com" => 0, // #965
     "cc.map.qq.com" => 0, // #974
+    "pkoplink.com" => 0, // #998
 
     "iflow.uczzd.cn" => 0, // uc搞笑段子
     "m.uczzd.cn" => 0, // uc搞笑段子
