18093937a17d229976e8aacc3228cd1f33e5f2c5
GitHub Action <action@github.com>
2024-12-14 05:28:38 +0800

    fix 1002

--- scripts/tools/easylist-extend.php
+++ scripts/tools/easylist-extend.php
@@ -310,6 +310,7 @@ $ARR_WHITE_RULE_LIST = [
     '@@||usageapi.*.oraclecloud.com^' => 1, // #961
     '@@||track.vontron.com^' => 1, // #985
     '@@||download.falco.org^' => 1, // #993
+    '@@||ad-block.dns.adguard.com^' => 1, // #1002
 ];
 
 //針對上游赦免規則anti-AD不予赦免的規則，即赦免名單的黑名單
