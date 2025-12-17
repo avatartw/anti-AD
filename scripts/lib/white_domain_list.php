04e830ae445b9200291ec4186ab60e9196478a49
GitHub Action <action@github.com>
2025-12-17 14:39:14 +0800

    fix 1089

--- scripts/lib/white_domain_list.php
+++ scripts/lib/white_domain_list.php
@@ -473,6 +473,7 @@ return [
     "scarf.sh" => 0, // #957, #993, #1076
     "ads.privacy.qq.com" => 0, // #546
     "dig.lianjia.com" => 0, // #1088
+    "systeme.io" => 0, // #1089
 
     // upstream-white-easylist 中选取的一些应放行域名
     "a1.itc.cn" => 0,
